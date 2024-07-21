84d73b40fd13cffc57068511aca4ba7051149927
Alexandre Diniz <zinidgames@gmail.com>
2024-07-21 02:05:39 -0300

    Formulário de contato

---
+++ scripts/contato.php
@@ -0,0 +1,18 @@
+<?php
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $msg = addslashes($_POST['msg']);
+
+    $destinatario = "zinidgames@gmail.com";
+    $assunto = "Prospecção de cliente - Agência DNZ";
+
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$telefone;
+    $cabecalho = "From: contato@agenciadnz.com.br"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion()
+
+    if(mail($destinatario,$assunto,$corpo,$cabecalho)) {
+        echo("Email enviado com sucesso!");
+    } else {
+        echo("[ERRO] Sua mensagem não foi enviada, verifique os dados e tente novamente!");
+    }
+?>
